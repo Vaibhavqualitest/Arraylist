30560eb8cff955ea457811f105c043aa28883751
Vaibhav Bajpai <vaibhav.bajpai@QUALITEST.LOCAL>
2021-09-03 16:33:14 +0530

    Third commit

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,17 +1,17 @@
 import java.util.*;
 public class ArrayTest {
 	public static void main(String[] args) {
-		System.out.println("Create operations");
+		System.out.println("Create operation");
 		ArrayList<Integer> myNumbers = new ArrayList<Integer>();
 		myNumbers.add(1);
 		myNumbers.add(2);
 		myNumbers.add(3);
 		myNumbers.add(4);
-		System.out.println("Read Operations");
+		System.out.println("Read Operation");
 		for(int i: myNumbers) {
 			System.out.println(i);
 		}
-		System.out.println("Remove Operations");
+		System.out.println("Remove Operation");
 		myNumbers.remove(2);
 		System.out.println("now numbers are");
 		for(int i: myNumbers) {
